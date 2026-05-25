3506b64989ab5131d5aff0ff565509ee2c4523ea
Saab JAS 39 Gripen <47117131+iliketheinternet223@users.noreply.github.com>
2026-05-25 17:09:33 -0300

    meu deus não sobrou nada

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const menu = document.getElementById("Menu1");
 
 botao1.addEventListener("click", () => {
     menu.classList.toggle("ativo");
+
+    menu4.classList.remove("ativo");
     menu2.classList.remove("ativo");
     menu3.classList.remove("ativo");
+    menu5.classList.remove("ativo");
 });
 
 const botao2 = document.getElementById("dieta2");
@@ -12,8 +15,11 @@ const menu2 = document.getElementById("Menu2");
 
 botao2.addEventListener("click", () => {
     menu2.classList.toggle("ativo");
+
     menu.classList.remove("ativo");
+    menu4.classList.remove("ativo");
     menu3.classList.remove("ativo");
+    menu5.classList.remove("ativo");
 });
 
 const botao3 = document.getElementById("dieta3");
@@ -21,6 +27,33 @@ const menu3 = document.getElementById("Menu3");
 
 botao3.addEventListener("click", () => {
     menu3.classList.toggle("ativo");
+
     menu.classList.remove("ativo");
     menu2.classList.remove("ativo");
+    menu4.classList.remove("ativo");
+    menu5.classList.remove("ativo");
+});
+
+const botao4 = document.getElementById("dieta4");
+const menu4 = document.getElementById("Menu4");
+
+botao4.addEventListener("click", () => {
+    menu4.classList.toggle("ativo");
+
+    menu.classList.remove("ativo");
+    menu2.classList.remove("ativo");
+    menu3.classList.remove("ativo");
+    menu5.classList.remove("ativo");
+});
+
+const botao5 = document.getElementById("dieta5");
+const menu5 = document.getElementById("Menu5");
+
+botao5.addEventListener("click", () => {
+    menu5.classList.toggle("ativo");
+
+    menu.classList.remove("ativo");
+    menu2.classList.remove("ativo");
+    menu3.classList.remove("ativo");
+    menu4.classList.remove("ativo");
 });
